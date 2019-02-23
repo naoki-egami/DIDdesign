f38ad275e95cc6b1bef24d6e7637d966a9f85aad
sou412 <sou412snow@gmail.com>
2019-02-22 20:36:03 -0500

    add summary function

---
+++ R/did-summary.R
@@ -0,0 +1,243 @@
+
+##
+## Summary and plot functions
+##
+
+summarize.diddesign <- function(data) {
+  n_post <- length(data)
+  ## get some data
+  treat  <- data[[1]]$D
+  n_obs  <- length(treat)
+  t_time <- ncol(data[[1]]$Y)
+  Y_pre  <- data[[1]]$Y[,-t_time]
+  id_time <- attr(data[[1]], 'id_time')
+
+  if (n_post > 1) {
+    Y_post <- matrix(NA, nrow = n_obs, ncol = n_post)
+    for (i in 1:n_post) {
+      Y_post[,i] <- data[[i]]$Y[,t_time]
+      id_time <- c(id_time, attr(data[[i]], 'post_treat'))
+    }
+    id_time <- unique(id_time)
+  } else {
+    Y_post <- data[[1]]$Y[,t_time]
+  }
+
+  ## compute mean outcome
+  Y <- cbind(Y_pre, Y_post)
+  ymean <- apply(Y, 2, function(x) tapply(x, treat, mean, na.rm = TRUE))
+
+  return(list("ymean" = ymean, 'id_time' = id_time))
+
+}
+
+#' Summary Function
+#' @export
+summary.diddesign <- function(obj) {
+  if ('diddesign_data' %in% class(obj)) {
+    summary_dat <- summarize.diddesign(obj)
+    post_first <- attr(obj[[1]], 'post_treat')
+    post_bool  <- summary_dat$id_time >= post_first
+    status <- ifelse(post_bool, "T", "C")
+    res_tab <- data.frame(rbind(
+      status, round(summary_dat$ymean[1,], 2), round(summary_dat$ymean[2,], 2),
+      round(summary_dat$ymean[1,] - summary_dat$ymean[2,], 2)
+    ), row.names =  c("Status", "Y:Treated", "Y:Control", "YT-YC"))
+
+    colnames(res_tab) <- summary_dat$id_time
+    # rownames(res_tab) <- c("Status", "Y:Treated", "Y:Control")
+  } else if ('diddesign' %in% class(obj)){
+    ATT <- sapply(obj, function(x) round(x$ATT, 3))
+    BIC <- sapply(obj, function(x) round(x$BIC.min, 3))
+    HQIC <- sapply(obj, function(x) round(x$HQIC.min, 3))
+    selected <- paste("M", sapply(obj, function(x) as.character(x$min_model)), sep = '')
+
+    if (isTRUE(attr(obj[[1]], 'boot'))) {
+      se_save <- rep(NA, length(ATT))
+      for (i in 1:length(ATT)) {
+        se_save[i] <- paste("[", paste(round(obj[[i]]$ci95, 3), collapse = ', '), "]", sep = '')
+      }
+
+      # did estimates
+      if (!is.null(obj[[1]]$results_standardDiD)) {
+        DiD <- sapply(obj, function(x) round(x$results_standardDiD$ATT, 3))
+        DiD_se_save <- rep(NA, length(DiD))
+        for (i in 1:length(ATT)) {
+          DiD_se_save[i] <- paste("[",
+            paste(round(obj[[i]]$results_standardDiD$results_bootstraps$ci95, 3),
+            collapse = ', '), "]", sep = '')
+        }
+
+        # make a table
+        tabs_var <- c("D-DiD", "", "", "", "", "Std-DiD", "")
+        labs_var <- c("ATT", "95% CI", "BIC", "HQIC", "Selected", "ATT",  "95% CI")
+        res_tab <- data.frame(cbind(tabs_var, labs_var,
+          rbind(ATT, se_save, BIC, HQIC, selected, DiD, DiD_se_save))
+        )
+
+        # col labels
+        colnames(res_tab) <- c("", "", sapply(obj, function(x) attr(x, 'post_treat')))
+        rownames(res_tab) <- NULL
+      } else {
+        res_tab <- data.frame(rbind(
+          ATT, se_save, BIC, HQIC, selected
+        ), row.names = c("ATT", "95% CI", "BIC", "HQIC", "Selected Model"))
+        colnames(res_tab) <- sapply(obj, function(x) attr(x, 'post_treat'))
+      }
+
+    } else {
+      res_tab <- data.frame(rbind(
+        ATT, BIC, HQIC, selected
+      ), row.names = c("ATT", "BIC", "HQIC", "Selected Model"))
+      colnames(res_tab) <- sapply(obj, function(x) attr(x, 'post_treat'))
+
+    }
+  }
+
+  return(res_tab)
+}
+
+
+
+#' Plot function
+#' @param data Input object. This should be either did_double object or did_double.data object.
+#'  If did_double object is provided, ATT estimates with 95% confidence intervals are plotted.
+#'  If did_double.data objec is provided, mean outcomes for the treated and the control are plotted.
+#' @param full A boolean argument, if \code{TRUE} ATT estimates for all models are plotted,
+#'  otherwise only estimates from the selected model will be shown. Default is \code{FALSE}.
+#' @param xlim xlim in plot function. If left \code{NULL}, it's automatically set.
+#' @param ylim ylim in plot function. If left \code{NULL}, it's automatically set.
+#' @param ... additional arguments supplied to the plot function.
+#' @export
+plot.diddesign <- function(data, xlim = NULL, ylim = NULL, full = FALSE, ...) {
+  if('did_double.data' %in% class(data)) {
+    ##
+    ## plot for raw data
+    ##
+    summary_dat <- summarize.diddesign(data)
+    ymean <- summary_dat$ymean
+    id_time <- summary_dat$id_time
+
+    ## plot
+    if (is.null(xlim)) xlim <- c(min(id_time), max(id_time))
+    if (is.null(ylim)) ylim <- c(min(ymean), max(ymean))
+    time_use <- which(attr(data[[1]], 'post_treat') == id_time)
+    plot(1, 1, ylim = ylim, xlim = xlim,
+      xlab = "", ylab = "Mean Outcome", ...)
+    abline(v = mean(id_time[(time_use-1):time_use]), col = 'red', lty = 3, lwd = 1.3)
+    lines(id_time, ymean[1,], col = '#006284', pch = 16, type = 'b', lwd = 1.5)
+    lines(id_time, ymean[2,], col = 'gray60',  pch = 17, type = 'b', lwd = 1.5)
+    legend('topleft', legend = c("treated", 'control'), col = c('#006284', 'gray60'),
+      lty = 1, pch = c(16, 17), bty = 'n')
+
+  } else if ('did_double' %in% class(data)) {
+    ##
+    ## plot for estimation result
+    ##
+    ATT <- sapply(data, function(x) round(x$ATT, 3))
+    xlab <- sapply(data, function(x) attr(x, 'post_treat'))
+    if (isTRUE(attr(data[[1]], 'boot') & isTRUE(full)) &
+        !is.null(data[[1]]$results_standardDiD)) {
+      se_list <- ATT_list<- list()
+      for (i in 1:length(data)) {
+        se_mat <- matrix(NA, nrow = length(data[[i]]$results_bootstraps) + 1, ncol = 4)
+        att_vec <- rep(NA, length(data[[i]]$results_bootstraps) + 1)
+
+        ## save DiD
+        se_mat[1,1:2] <- data[[i]]$results_standardDiD$results_bootstraps$ci90
+        se_mat[1,3:4] <- data[[i]]$results_standardDiD$results_bootstraps$ci95
+        att_vec[1] <- data[[i]]$results_standardDiD$ATT
+
+        for (j in 1:length(data[[i]]$results_bootstraps)) {
+          se_mat[j+1,1:2] <- data[[i]]$results_bootstraps[[j]]$ci90
+          se_mat[j+1,3:4] <- data[[i]]$results_bootstraps[[j]]$ci95
+          att_vec[j+1]    <- data[[i]]$results_estimates[[j]]$ATT
+        }
+        se_list[[i]] <- se_mat
+        ATT_list[[i]] <- att_vec
+      }
+
+      ## plot
+      points_master <- c(16:17, 15, 2, 4:9)
+      locs <- seq(-0.3, 0.3, length.out = length(data[[1]]$results_bootstraps)+1)
+      points <- c(1, points_master[1:length(data[[1]]$results_bootstraps)])
+
+      if (is.null(xlim)) xlim <- c(0.5, length(ATT)+0.5)
+      if (is.null(ylim)) ylim <- c(min(unlist(se_list)), max(unlist(se_list)))
+      plot (1, 1, pch = 16, ylim = ylim, xlim = xlim, xaxt = "n", ylab = "ATT", xlab = "", ...)
+      axis(1, at = 1:length(ATT), xlab)
+      for (i in 1:length(data)) {
+        for (j in 1:(length(data[[i]]$results_bootstraps)+1)) {
+          # point
+          points(i+locs[j], ATT_list[[i]][j], pch = points[j])
+          # 90%
+          lines(c(i+locs[j],i+locs[j]), c(se_list[[i]][j,1], se_list[[i]][j,2]), lwd = 2.2)
+          # 95%
+          arrows(i+locs[j], se_list[[i]][j,3], i+locs[j], se_list[[i]][j,4], length=0.05, angle=90, code=3)
+
+        }
+      }
+      legend("topleft", legend = c("DiD", paste("M", 1:length(data[[1]]$results_bootstraps))),
+        pch = points, lty = 1, bty = 'n')
+
+    } else if (isTRUE(attr(data[[1]], 'boot') & isTRUE(full))) {
+      se_list <- ATT_list<- list()
+      for (i in 1:length(data)) {
+        se_mat <- matrix(NA, nrow = length(data[[i]]$results_bootstraps), ncol = 4)
+        att_vec <- rep(NA, length(data[[i]]$results_bootstraps))
+        for (j in 1:length(data[[i]]$results_bootstraps)) {
+          se_mat[j,1:2] <- data[[i]]$results_bootstraps[[j]]$ci90
+          se_mat[j,3:4] <- data[[i]]$results_bootstraps[[j]]$ci95
+          att_vec[j] <- data[[i]]$results_estimates[[j]]$ATT
+        }
+        se_list[[i]] <- se_mat
+        ATT_list[[i]] <- att_vec
+      }
+
+      ## plot
+      points_master <- c(16:17, 1:9)
+      locs <- seq(-0.3, 0.3, length.out = length(data[[1]]$results_bootstraps))
+      points <- points_master[1:length(data[[1]]$results_bootstraps)]
+
+      if (is.null(xlim)) xlim <- c(0.5, length(ATT)+0.5)
+      if (is.null(ylim)) ylim <- c(min(unlist(se_list)), max(unlist(se_list)))
+      plot (1, 1, pch = 16, ylim = ylim, xlim = xlim, xaxt = "n", ylab = "ATT", xlab = "", ...)
+      axis(1, at = 1:length(ATT), xlab)
+      for (i in 1:length(data)) {
+        for (j in 1:length(data[[i]]$results_bootstraps)) {
+          # point
+          points(i+locs[j], ATT_list[[i]][j], pch = points[j])
+          # 90%
+          lines(c(i+locs[j],i+locs[j]), c(se_list[[i]][j,1], se_list[[i]][j,2]), lwd = 2.2)
+          # 95%
+          arrows(i+locs[j], se_list[[i]][j,3], i+locs[j], se_list[[i]][j,4], length=0.05, angle=90, code=3)
+
+        }
+      }
+      legend("topleft", legend = paste("M", 1:length(data[[1]]$results_bootstraps)),
+        pch = points, lty = 1, bty = 'n')
+
+    } else if (isTRUE(attr(data[[1]], 'boot'))) {
+      se_mat <- matrix(NA, nrow = length(data), ncol = 4)
+      for (i in 1:length(data)) {
+        se_mat[i,1:2] <- data[[i]]$ci90
+        se_mat[i,3:4] <- data[[i]]$ci95
+      }
+
+      ## plot
+      if (is.null(xlim)) xlim <- c(0.5, length(ATT)+0.5)
+      if (is.null(ylim)) ylim <- c(min(se_mat), max(se_mat))
+      plot (ATT, pch = 16, ylim = ylim, xlim = xlim, xaxt = "n", xlab = "", ...)
+      axis(1, at = 1:length(ATT), xlab)
+      for (i in 1:length(data)) {
+        # 90%
+        lines(c(i,i), c(se_mat[i,1], se_mat[i,2]), lwd = 2.2)
+        # 95%
+        arrows(i, se_mat[i,3], i, se_mat[i,4], length=0.05, angle=90, code=3)
+      }
+
+    } else {
+      stop("Plot function is not suppored for estimates without bootstrap se's")
+    }
+  }
+}
--- R/did_data.R
+++ R/did_data.R
@@ -1,7 +1,195 @@
 
 
-#' Prepare Data for did Function 
-#' @export 
-did_data <- function(outcome) {
-  return(outcome)
+#' Transform data into did_double format
+#'
+#' A function to transform panel data into did double format.
+#'
+#' @param outcome Outcome vector when \code{long = TRUE} or outcome matrix when \code{long = FALSE}.
+#' @param treatment Treatment vector. When \code{long = TRUE}, the length of this vector should be the same as the length of \code{outcome} vector, while the length of \code{treatment} should be the same as the number of rows (i.e. the number of subjects) in \code{outcome} when the input is in the wide format.
+#' @param post_treatment A scalar or a vector of time index that indicats the post treatment period(s).
+#'  If left as \code{NULL}, the last period in a variable given as \code{id_time} is used as the post treatment period.
+#' @param id_subject A vector of subject index. Required if \code{long = TRUE}.
+#' @param id_time A vector of time index. Required if \code{long = TRUE}.
+#' @param long A boolean input indicating whether the input is in the long format or not.
+#' Default is \code{long = TRUE}.
+#' @return A \code{diddesign} class object.
+#' @importFrom dplyr %>% tbl_df select filter mutate
+#' @importFrom tidyr spread
+#' @importFrom plm pdata.frame
+#' @importFrom utils getFromNamespace
+#' @examples
+#'  # load data from didrobust package
+#'  data(anzia2012)
+#'
+#'  # convert data into did double format
+#'  out <- did_data(
+#'    outcome = anzia2012$lnavgsalary_cpi,
+#'    treatment = anzia2012$oncycle,
+#'    post_treatment = c(2007, 2008, 2009),
+#'    id_subject = anzia2012$district,
+#'    id_time = anzia2012$year,
+#'    long = TRUE
+#'  )
+#'
+#'  # make a plot
+#'  plot(out)
+#'
+#'  # view a summary
+#'  summary(out)
+#' @export
+did_data <- function(
+  outcome, treatment,
+  post_treatment = NULL, id_subject = NULL, id_time = NULL, long = TRUE,
+  Xcov = NULL
+) {
+  if (isTRUE(long)) {
+    ## if the input is in the long format,
+    ## we transform it to the wide format
+
+    ## na omit
+    outcome <- na.omit(outcome); treatment <- na.omit(treatment)
+
+    ## export function
+    diff.pseries <- getFromNamespace("diff.pseries", "plm")
+
+    ### ==== intput check ==== ###
+    if (length(outcome) != length(treatment)) {
+      stop("Length of outcome and length of treatment do not match.")
+    }
+    if (any(c(min(treatment) != 0, max(treatment) != 1))) {
+      stop("We currently support the binary treatment. treatment vector should take either 0 or 1.")
+    }
+    if (any(c(is.null(id_subject)), is.null(id_time))) {
+      stop("`id_subject` and `id_time` should be provided when long = TRUE.")
+    } else {
+      id_subject <- na.omit(id_subject)
+      id_time <- na.omit(id_time)
+    }
+    if (!is.numeric(id_time)) {
+      ## this is a little sketchy conversion
+      ## maybe better to ask users to make the input numeric beforehand
+      id_time <- as.numeric(as.character(id_time))
+    }
+    if (is.null(post_treatment)) {
+      post_treatment <- max(id_time)
+      cat('We treat', post_treatment, 'as the post treatment period\n')
+    } else {
+      post_treatment <- sort(post_treatment)
+    }
+    if (min(length(outcome), length(treatment), length(id_subject), length(id_time))  !=
+        max(length(outcome), length(treatment), length(id_subject), length(id_time))) {
+      stop("outcome, treatment, id_subject and id_time should have the same length.")
+    }
+
+    if (!is.null(Xcov)) {  ## use this when Xcov is supplied
+      if (is.null(colnames(Xcov))) {
+        colnames(Xcov) <- paste("XR", 1:ncol(Xcov), sep = '')
+      }
+      if (any(colnames(Xcov) %in% c('', "", " ", ' '))) {
+        ## rename colnames if they contain empty name
+        rename_idx <- which(colnames(Xcov) %in% c('', "", " ", ' '))
+        colnames(Xcov)[rename_idx] <- paste('XR', length(rename_idx), sep = '')
+      }
+      x_colname <- colnames(Xcov)
+    }
+
+    ### ==== transform data ==== ###
+    dat <- data.frame(outcome, treatment, id_subject, id_time) %>% tbl_df()
+    out <- list()
+    for (tt in 1:length(post_treatment)) {
+      ## prep for nonparametric version
+      dat_tmp <- dat %>%
+        dplyr::filter(id_time < post_treatment[1] | id_time == post_treatment[tt]) %>%
+        na.omit()
+      y_tmp <- dat_tmp %>% dplyr::select(-treatment) %>% tidyr::spread(id_time, outcome) %>%
+        dplyr::select(-id_subject) %>% data.matrix()
+      d_tmp <- dat_tmp %>% select(-outcome) %>% tidyr::spread(id_time, treatment) %>%
+        dplyr::select(-id_subject) %>% data.matrix()
+
+      ## prep for parametric model
+
+      # create dynamic treatment variable
+      ## overwrite time index so that there's no time gap
+      ## this is necesary to take diff in plm
+      if (is.null(Xcov)) {
+        dat2 <- dat %>%
+          dplyr::filter(id_time < post_treatment[1] | id_time == post_treatment[tt]) %>%
+          mutate(id_time = ifelse(id_time == post_treatment[tt], post_treatment[1], id_time)) %>%
+          na.omit()
+      } else {
+        dat2 <- data.frame(outcome, treatment, id_subject, id_time, Xcov) %>%
+          tbl_df() %>%
+          dplyr::filter(id_time < post_treatment[1] | id_time == post_treatment[tt]) %>%
+          mutate(id_time = ifelse(id_time == post_treatment[tt], post_treatment[1], id_time)) %>%
+          na.omit()
+
+      }
+      dat_plm <- pdata.frame(dat2, index = c("id_subject", "id_time"))
+
+      # make formula
+      pre_time <- ncol(y_tmp) - 1;
+      max_diff <- pre_time - 1 ## make sure this is positive
+      fm <- list()
+
+      for (i in 0:max_diff) {
+        if (is.null(Xcov)) {
+          fm[[i+1]] <- as.formula(paste("yd",i, "~ treatment", sep = ''))
+        } else {
+          fm[[i+1]] <- as.formula(paste("yd",i, " ~ treatment + ", paste(x_colname, collapse = "+"), sep = ""))
+        }
+        if (i > 0) {
+          dat_plm[paste("yd", i, sep = '')] <- diff.pseries(dat_plm$outcome, lag = i)
+        } else {
+          dat_plm[paste("yd", i, sep = '')] <- dat_plm$outcome
+        }
+
+      }
+
+      ## output
+      out[[tt]] <- list(
+        "Y" = y_tmp,
+        "D" = apply(d_tmp, 1, max),
+        'formula' = fm,
+        'pdata' = dat_plm
+      )
+
+      attr(out[[tt]], 'post_treat') <- post_treatment[tt]
+      attr(out[[tt]], 'id_time') <- sort(unique(id_time))
+    }
+
+  } else {
+    ## if the data is already in the wide format,
+    ## we will use the input as is
+
+    ## ==== input checks ==== ##
+    t_time <- ncol(Y); n_obs  <- nrow(Y)
+    if ((length(D) - n_obs) != 0) {
+      stop('Length of the treatment vector and the number of rows in Y do not match.')
+    }
+
+    ### ==== make data into did_double obj ==== ###
+    if (!is.null(post_treatment)) {
+      matched_var <- as.character(post_treatment) %in% colnames(outcome)
+      outcome <- outcome %>% tbl_df()
+      Ypre <- outcome %>% select(-post_treatment) %>% data.matrix()
+
+      if(any(matched_var)) {
+        out <- list()
+        Ypost <- outcome %>% select(post_treatment[matched_var]) %>% data.matrix()
+        for (i in 1:sum(matched_var)) {
+          out[[i]] <- list(
+            "Y" = cbind(Ypre, Ypost[,i]),
+            "D" = treatment
+          )
+        }
+      } else {
+        stop("colnames of outcome and variables in post_treatment do not match.")
+      }
+    } else {
+      stop("Please specify post_treatment by providing variable names.")
+    }
+  }
+
+  class(out) <- c("diddesign", "diddesign_data")
+  return(out)
 }
