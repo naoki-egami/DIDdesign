d123523d8ad2fe909f6afae57e1cd8fcb00b27b1
sou412 <sou412snow@gmail.com>
2020-12-14 18:34:53 -0500

    Add minor fix to address warnings

--- R/did-summary.R
+++ R/did-summary.R
@@ -6,17 +6,18 @@
 
 
 #' Summarize DIDdesign output
+#' @param object 
 #' @export
-summary.DIDdesign <- function(obj) {
+summary.DIDdesign <- function(object) {
 
-  if (!("DIDdesign" %in% class(obj))) stop("obj should the output of did funciton.")
-  out <- obj$estimates
+  if (!("DIDdesign" %in% class(object))) stop("object should the output of did funciton.")
+  out <- object$estimates
   out$statistic <- out$estimate / out$std.error
   out$p_value   <- 2 * pnorm(abs(out$statistic), lower.tail = FALSE)
 
   out <- out[, c("estimator", "lead", "estimate", "std.error", "statistic", "p_value", "ddid_weight")]
   # ## add weights
-  # weights <- c(NA, obj$weights$weight_did, obj$weights$weight_sdid)
+  # weights <- c(NA, object$weights$weight_did, object$weights$weight_sdid)
   # out$ddid_weights <- weights
 
   class(out) <- c(class(out), "summary.DIDdesign.")
@@ -27,9 +28,9 @@ summary.DIDdesign <- function(obj) {
 #' Print
 #' @export
 #' @importFrom cli cat_rule
-#' @param obj An object of \code{summary.DIDdesign} class. This is typically an output of \code{summary.DIDdesign()} function.
-print.summary.DIDdesign <- function(obj) {
+#' @param x An object of \code{summary.DIDdesign} class. This is typically an output of \code{summary.DIDdesign()} function.
+print.summary.DIDdesign <- function(x, ...) {
   cat_rule(left = crayon::bold("Estimates"))
-  print(obj)
-  invisible(obj)
+  print(x)
+  invisible(x)
 }
